f1dbef0eda857b44dd4496f50bd7b08df2ee8786
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-11-06 20:45:15 +0300

    task/009 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,60 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import kotlinx.parcelize.Parcelize
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    @Parcelize
+    open class State : Parcelable {
+        open fun apply(linearLayout: LinearLayout, textView: TextView) = Unit
+
+        object Initial : State()
+
+        object Removed : State() {
+            override fun apply(linearLayout: LinearLayout, textView: TextView) {
+                linearLayout.removeView(textView)
+            }
+        }
+    }
+
+    private var state: State = State.Initial
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        linearLayout = findViewById(R.id.rootLayout)
+        textView = findViewById(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.removeButton)
+        button.setOnClickListener {
+            state = State.Removed
+            state.apply(linearLayout, textView)
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            savedInstanceState.getParcelable(KEY, State::class.java) as State
+        else
+            savedInstanceState.getParcelable<State>(KEY) as State
+
+        state.apply(linearLayout, textView)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putParcelable(KEY, state)
+        super.onSaveInstanceState(outState)
+    }
+
+    private companion object {
+        const val KEY = "key"
     }
 }
