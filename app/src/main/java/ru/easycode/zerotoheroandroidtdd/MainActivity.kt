6fe2c51832b59fa987c5a6d6685f1e3e4ff090af
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-01-09 16:00:15 +0300

    task 21 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,26 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val textWatcher = binding.inputEditText.addTextChangedListener {
+            binding.actionButton.isEnabled = it.toString().length > 2
+        }
+
+        binding.actionButton.setOnClickListener {
+            binding.titleTextView.text = binding.inputEditText.text
+            binding.inputEditText.removeTextChangedListener(textWatcher)
+            binding.inputEditText.text?.clear()
+            binding.inputEditText.addTextChangedListener(textWatcher)
+        }
     }
 }
