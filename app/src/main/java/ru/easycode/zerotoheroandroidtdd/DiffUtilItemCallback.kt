d6df65fa841871aadf9bb7aae95552515309b2b7
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-01-18 11:53:59 +0300

    task 24 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/DiffUtilItemCallback.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/DiffUtilItemCallback.kt
@@ -5,18 +5,10 @@ import androidx.recyclerview.widget.DiffUtil
 class DiffUtilItemCallback : DiffUtil.ItemCallback<CharSequence>() {
 
     override fun areItemsTheSame(oldItem: CharSequence, newItem: CharSequence): Boolean {
-        return oldItem == newItem
+        return oldItem.contentEquals(newItem)
     }
-
 
     override fun areContentsTheSame(oldItem: CharSequence, newItem: CharSequence): Boolean {
-        return oldItem == newItem
+        return oldItem.contentEquals(newItem)
     }
-}
-
-fun main() {
-    val text1: CharSequence = "123"
-    val text2: CharSequence = "124"
-
-    println(text1 == text2)
 }
